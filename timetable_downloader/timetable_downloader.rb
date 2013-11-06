c7249a24b26f098f0612364b447636e3399765d7
Sam Giffney <samuelgiffney@gmail.com>
2013-11-07 08:39:35 +1100

    fix for October 20 major timetable changes

--- timetable_downloader/timetable_downloader.rb
+++ timetable_downloader/timetable_downloader.rb
@@ -20,36 +20,41 @@ end
 def get_departure_times(route_code, date)
   url = "http://www.131500.com.au/plan-your-trip/timetables/@@details?line=#{route_code}&itd_itdDate=#{date}"
   # http://www.131500.com.au/plan-your-trip/timetables/@@details?line=NSW:09MAN:L:H:SJ2&itd_itdDate=20131225
+  print "#{url}\n"
   page = getPage(url)
   extract_departure_times(page)
 end
 
 def download_timetables(local_file)
+  # route code by clicking on show timetable for route at http://www.131500.com.au/plan-your-trip/timetables/ and getting line url param
   routes = {}
-  routes['NSW:09MAN:L:H:SJ2'] = 'To Manly:Slow Ferry' #Circular Quay to Manly'
+  routes['NSW:090F1:_:H:SJ2'] = 'To Manly:Slow Ferry' #Circular Quay to Manly'
   routes['NSW:55001:_:H:SJ2'] = 'To Manly:Manly Fast Ferry' #Circular Quay to Manly (Manly Fast Ferry)'
   routes['NSW:56001:_:H:SJ2'] = 'To Manly:Sydney Fast Ferries' #Circular Quay to Manly (Sydney Fast Ferry)'
-  routes['NSW:09MAN:L:R:SJ2'] = 'From Manly:Slow Ferry' #Manly to Circular Quay'
+  routes['NSW:090F1:_:R:SJ2'] = 'From Manly:Slow Ferry' #Manly to Circular Quay'
   routes['NSW:55001:_:R:SJ2'] = 'From Manly:Manly Fast Ferry' #Manly to Circular Quay (Manly Fast Ferry)'
   routes['NSW:56001:_:R:SJ2'] = 'From Manly:Sydney Fast Ferries' #Manly to Circular Quay (Sydney Fast Ferry)'
 
   dates = {} # some typical non public holiday dates, must be future dated and < 90 days
-  dates['mon'] = '20130729'
-  dates['tue'] = '20130730'
-  dates['wed'] = '20130731'
-  dates['thu'] = '20130801'
-  dates['fri'] = '20130802'
-  dates['sat'] = '20130803'
-  dates['sun'] = '20130804'
-  dates['hol'] = '20131007'
+  dates['mon'] = '20131111'
+  dates['tue'] = '20131110'
+  dates['wed'] = '20131112'
+  dates['thu'] = '20131113'
+  dates['fri'] = '20131114'
+  dates['sat'] = '20131115'
+  dates['sun'] = '20131116'
+  dates['hol'] = '20140127'
 
   departure_times = {}
 
   dates.each{ |day_of_week, date|
+    print "Downloading #{date}\n"
     departure_times[day_of_week] = {}
     routes.each{ |route_code, route_name|
+      print "Route #{route_code} #{route_name}\n"
       times = get_departure_times(route_code, date)
       departure_times[day_of_week][route_name] = times
+      print "#{times}\n\n"
     }
   }
 
